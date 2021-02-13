9895d794e19924a25b195a294befa56c1e0e83c7
igor <igorborges812@gmail.com>
2021-02-13 00:54:32 -0300

    tela13/02

--- login.php
+++ login.php
@@ -3,45 +3,59 @@
 
 <head>
     <link rel="stylesheet" href="style.php" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
 
 <body>
-        <div class="container">
-                <div class="content first-content">
-                <div class="first-column">
-                <h2 class="title title-welcome">welcome back!</h2>
-                <p class="description description-primary">To keep connected with us</p>
-                <p class="description description-primary">please login with your personal info</p>
-                <button class="btn btn-primary">sign up</button>
-        </div>
-         
-            <form class="form"> 
-            <div class="second-column"> 
-            <h2 class="title title-primary">Entrar no Sistema</h2>
+    <div class="container">
+        <div class="content first-content">
+            <div class="first-column">
+                <h2 class="title title-first">Bem Vindo!</h2>
+                <p class="description description-primary">Caso não possua uma conta, cadastre-se</p>
+                <p class="description description-primary">com suas informações pessoais.</p>
+                <button id="signin" class="btn btn-primary">Cadastrar</button>
+
+            </div>
+
+            <form class="form">
+                <div class="second-column">
+                    <h2 class="title title-second">Entrar no Sistema</h2>
+
                     <label class="label-input" for="">
-                    <i class="far fa-envelope icon-modify"></i>
-                    <input type="email" placeholder="Email">
+                        <i class="far fa-envelope icon-modify"></i>
+                        <input type="email" placeholder="Email">
                     </label>
+
                     <label class="label-input" for="">
-                    <i class="fas fa-lock icon-modify"></i>
-                    <input type="password" placeholder="Password">
+                        <i class="fas fa-lock icon-modify"></i>
+                        <input type="password" placeholder="Senha">
                     </label>
-                    <button class="btn btn-second">sign up</button>   
-                    </div>     
+                    <button class="btn btn-second">Entrar</button>
+                </div>
             </form>
-                
+            
         </div>
             <div class="content second-content">
-            <div class="first-column">
-                <h2 class="title">hello, friend!</h2>
-                <button class="btn">sign up</button>
-                <div class="content second-content">
                 <div class="first-column">
-                <h2 class="title title-primary">hello, friend!</h2>
-                <p class="description description-primary"><p><a href="servidor.php">Servidor</a></p> 
-                <p class="description description-primary"><p><a href="servidor.php">Professor</a></p> 
-                <p class="description description-primary"><p><a href="servidor.php">Aluno</a></p> 
-        </div>
-         
+                    <h2 class="title title-first">Bem Vindo!</h2>
+                    <p class="description description-primary">Caso já possua uma conta,  </p>
+                    <p class="description description-primary">faça o seu login.</p>
+                    <button id="signup"class="btn btn-second">Login</button>
+
+                </div>
+
+                <div class="second-column">
+                    <h2 class="title title-second">Faça o seu Cadastro!</h2>
+                    <p class="description description-second">
+                    <p><a href="servidor.php"><button class="btn btn-second">Servidor</button></a></p>
+                    <p class="description description-second">
+                    <p><a href="professor.php"><button class="btn btn-second">Professor</button></a></p>
+                    <p class="description description-second">
+                    <p><a href="aluno.php"><button class="btn btn-second">Aluno</button></a></p>
+                </div>
+            </div>
+    </div>
+    <script src="js/app.js"></script>
 </body>
+
 </html>
--- style.php
+++ style.php
@@ -20,8 +20,8 @@ body {
 .content{
     border-radius:15px;
     background-color:#FFFFFF;
-    width: 80%;
-    height: 50%;
+    width: 960px;
+    height: 60%;
     justify-content:space-between;
     align-items:center;
     position:relative;
@@ -29,21 +29,25 @@ body {
 .content::before {
     content: "";
     position: absolute;
-    background-color: #74D8FD;
+    background-color: #4D8C78;
     width: 40%;
     height: 100%;
     border-top-left-radius: 15px;
     border-bottom-left-radius: 15px;
-    
+    left:0;
+ 
 }
 .title{
     font-size:28px;
     font-weight:bold;
-    text-transform:capitalize;
 }
 
-.title-welcome{
+.title-first{
     color:#FFF;
+    
+}
+.title-second{
+    color:#4D8C78;
     
 }
 .description{
@@ -60,36 +64,265 @@ body {
     padding:10px 50px;
     cursor: pointer;
     font-weight:bold;
+    width:150px;
+    align-self:center;
+    border:none;
+    margin-top:1rem;
+    
+   
 }
 .btn-primary{
     background-color: transparent;
     border: 1px solid #FFF;
+    margin-top:70px;
+    
 }
 .btn-primary:hover{
     background-color: #FFF;
-    color:#74D8FD;  
+    color:#4D8C78;  
 }
-.first-content {
-display:flex;
+.btn-second {
+    background-color: #4D8C78;
+    border: 1px solid #4D8C78;
+    transition: background-color .5s;
+    border-color:#FFF;
+    text-align:center;
+    
+}
+.btn-second:hover {
+    background-color: #fff;
+    border: 1px solid #4D8C78;
+    color: #4D8C78;
 }
 
+
+.first-content {
+display:flex;
+
+}
+.first-content .second-column{
+    z-index:11;
+}
 .first-column {
     text-align:center;
     width: 40%;
     z-index: 10;
 }
+
 .second-column{
     width: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     
 }
 
 .form {
     display:flex;
     flex-direction:column;
+    width:55%;
+    
 }
 
+.form input {
+    height: 45px;
+    width: 100%;
+    border: none;
+    background-color: #ecf0f1;
+   
+     }
+     
+     input:-webkit-autofill 
+{    
+    -webkit-box-shadow: 0 0 0px 1000px #ecf0f1 inset !important;
+    -webkit-text-fill-color: #000 !important;
+ }
+
+.label-input {
+    background-color: #ecf0f1;
+    display: flex;
+    align-items: center;
+    margin:8px;
+
+}
+.icon-modify {
+    color: #7f8c8d;
+    padding: 0 5px;
+    }
 
 .second-content {
 position: absolute;
-display:none;
+display:flex;
+}
+
+.second-content .first-column {
+    order: 2;  
+  z-index:-1;
+}
+.second-content .second-column {
+    order: 1;   
+    z-index:-1;
+}
+
+.sign-in-js .first-content .first-column {
+    z-index: -1;
+}
+
+.sign-in-js .second-content .second-column {
+    z-index: 11;
+}
+.sign-in-js .second-content .first-column {
+    z-index: 13;
+}
+
+.sign-in-js .content::before {
+    left: 60%;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+    animation: slidein 1.3s; /*MODIFIQUEI DE 3s PARA 1.3s*/
+
+    z-index: 12;
+}
+
+.sign-up-js .content::before {
+    animation: slideout 1.3s; /*MODIFIQUEI DE 3s PARA 1.3s*/
+
+    z-index: 12;
+}
+
+.sign-up-js .second-content .first-column,
+.sign-up-js .second-content .second-column {
+    z-index: -1;
+}
+
+.sign-up-js .first-content .second-column {
+    z-index: 11;
+}
+
+.sign-up-js .first-content .first-column {
+    z-index: 13;    
+}
+
+.sign-in-js .first-content .second-column {
+
+    z-index: -1;
+    position: relative;
+    animation: deslocamentoEsq 1.3s; /*MODIFIQUEI DE 3s PARA 1.3s*/
+}
+
+.sign-up-js .second-content .second-column {
+    position: relative;
+    z-index: -1;
+    animation: deslocamentoDir 1.3s; /*MODIFIQUEI DE 3s PARA 1.3s*/
+}
+
+@keyframes deslocamentoEsq {
+
+    from {
+        left: 0;
+        opacity: 1;
+        z-index: 12;
+    }
+
+    25% {
+        left: -80px;
+        opacity: .5;
+        /* z-index: 12; NÃO HÁ NECESSIDADE */
+    }
+
+    50% {
+        left: -100px;
+        opacity: .2;
+        /* z-index: 12; NÃO HÁ NECESSIDADE */
+    }
+
+    to {
+        left: -110px;
+        opacity: 0;
+        z-index: -1;
+    }
+}
+
+
+@keyframes deslocamentoDir {
+
+    from {
+        left: 0;
+        z-index: 12;
+    }
+
+    25% {
+        left: 80px;
+        /* z-index: 12;  NÃO HÁ NECESSIDADE*/
+    }
+
+    50% {
+        left: 100px;
+        /* z-index: 12; NÃO HÁ NECESSIDADE*/
+        /* background-color: yellow;  Exemplo que dei no vídeo*/
+    }
+
+    to {
+        left: 110px;
+        z-index: -1;
+    }
+}
+
+
+@keyframes slidein {
+
+    from {
+        left: 0;
+        width: 40%;
+    }
+
+    25% {
+        left: 5%;
+        width: 50%;
+    }
+
+    50% {
+        left: 25%;
+        width: 60%;
+    }
+
+    75% {
+        left: 45%;
+        width: 50%;
+    }
+
+    to {
+        left: 60%;
+        width: 40%;
+    }
+}
+
+@keyframes slideout {
+
+    from {
+        left: 60%;
+        width: 40%;
+    }
+
+    25% {
+        left: 45%;
+        width: 50%;
+    }
+
+    50% {
+        left: 25%;
+        width: 60%;
+    }
+
+    75% {
+        left: 5%;
+        width: 50%;
+    }
+
+    to {
+        left: 0;
+        width: 40%;
+    }
 }
